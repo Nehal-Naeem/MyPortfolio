6f0fba76ca6978f9cabc2111035932de034b9570
Nehal <nehal.naeem2@gmail.com>
2021-12-09 15:45:36 -0800

    project grid

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,34 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+const projectsDemo = [{
+  title: 'ISS Live Locator',
+  description: ' Project Desc'
+},{
+  title: 'Data Snapshot',
+  description: ' Project Desc'
+},{
+  title: 'Weight Training Tracker',
+  description: ' Project Desc'
+},{
+  title: 'CLB React Dev',
+  description: ' Project Desc'
+}]
+
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projectsDemo.map((project) => (
+        <div>
+          {project.title}
+          <br />
+          {project.description}
+        </div>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
