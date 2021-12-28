18ef439f7980ea9edd6d1ed2462493b3963059ae
Nehal <nehal.naeem2@gmail.com>
2021-12-28 15:52:20 -0800

    finished technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { LinkContainer, Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="technologies">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      Primarily worked with web development technologies like Node.js, React, MongoDB. Some exposure to SQL, C++
+      and Java through coursework.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <LinkContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js 
+          </ListParagraph>
+        </LinkContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <LinkContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node and Databases 
+          </ListParagraph>
+        </LinkContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
